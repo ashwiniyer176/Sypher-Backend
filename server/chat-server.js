c031912f5c97265e27275724c8d9d0b2aa1573cf
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2021-05-21 10:57:39 +0530

    Connected to frontend Auth

---
+++ server/chat-server.js
@@ -0,0 +1,9 @@
+const WebSocket = require("ws");
+var models = require("./server.js").models;
+
+const ws = new WebSocket.Server({ port: 8080 });
+ws.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log("Got Message:", JSON.parse(message));
+  });
+});
