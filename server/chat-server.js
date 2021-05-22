d9294b4de4df5f9218397e66d75f2df2fbaf9372
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2021-05-22 18:02:35 +0530

    Fix Profile User relation bug

--- server/chat-server.js
+++ server/chat-server.js
@@ -3,7 +3,85 @@ var models = require("./server.js").models;
 
 const ws = new WebSocket.Server({ port: 8080 });
 ws.on("connection", (ws) => {
+  function login(email, pass) {
+    models.User.login(
+      {
+        email: email,
+        password: pass,
+      },
+      (err, res) => {
+        if (err) {
+          console.log("Error1:", err);
+          ws.send(
+            JSON.stringify({
+              type: "ERROR",
+              error: err,
+            })
+          );
+        } else {
+          console.log("Finding model");
+          models.User.findOne(
+            { where: { id: res.userId }, include: "Profile" },
+            (err2, user) => {
+              if (err2) {
+                console.log(err2);
+                ws.send(
+                  JSON.stringify({
+                    type: "ERROR",
+                    error: err2,
+                  })
+                );
+              } else {
+                console.log(user);
+                ws.send(
+                  JSON.stringify({
+                    type: "LOGGEDIN",
+                    data: {
+                      session: res,
+                      user: user,
+                    },
+                  })
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+  }
   ws.on("message", (message) => {
     console.log("Got Message:", JSON.parse(message));
+    let parsed = JSON.parse(message);
+    if (parsed) {
+      switch (parsed.type) {
+        case "SIGNUP":
+          models.User.create(parsed.data, (err, user) => {
+            if (err) {
+              ws.send(
+                JSON.stringify({
+                  type: "ERROR",
+                  error: err,
+                })
+              );
+            } else {
+              models.Profile.create(
+                {
+                  userId: user.id,
+                  name: parsed.data.name,
+                  email: parsed.data.email,
+                },
+                (profileError, profile) => {}
+              );
+            }
+          });
+          break;
+
+        case "LOGIN":
+          login(parsed.data.email, parsed.data.password);
+          break;
+        default:
+          console.log("Nothing to see here");
+      }
+    }
   });
 });
